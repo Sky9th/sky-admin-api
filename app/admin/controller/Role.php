b3db8a867285a46d2a11d8dae5aa038ebe0913b4
Sky9th <weitianxu@qq.com>
2020-02-03 12:32:05 +0800

    [+] 为管理组赋予权限 [+] 移除或添加用户到管理组

--- app/admin/controller/Role.php
+++ app/admin/controller/Role.php
@@ -2,6 +2,8 @@
 
 namespace app\admin\controller;
 
+use think\Exception;
+
 class Role extends Resource
 {
 
@@ -11,4 +13,49 @@ class Role extends Resource
         $this->validate = new \app\common\validate\sys\Role();
     }
 
+    /**
+     * 为管理组赋予权限
+     * @return array
+     * @throws
+     */
+    public function savePermission () {
+        $role_id = input('post.role_id');
+        $permission = input('post.permissions/a');
+        if($role_id == 1){
+            return error('超级管理组不允许修改以及删除');
+        }
+        try {
+            $this->model->startTrans();
+            $role = $this->model->find($role_id);
+            if ($role && $role->menus()->detach() !== false && $role->menus()->attach($permission) !== false ){
+                $this->model->commit();
+                return success('');
+            }
+            $this->model->rollback();
+            return error();
+        }catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * 移除或添加用户到管理组
+     * @return array
+     * @throws
+     */
+    public function modifyUser () {
+        $role_id = input('post.role_id');
+        $user_id = input('post.user_id');
+        $action =  input('post.action');
+        if ($role_id == 1 || $user_id == 1){
+            return error('超级管理组或超级管理员不允许修改以及删除');
+        }
+        $role = $this->model->find($role_id);
+        if ($action == 1) {
+            $res = $role->menus()->attach($user_id);
+        } else {
+            $res = $role->menus()->detach($user_id);
+        }
+        return $res ? success() : error();
+    }
 }
