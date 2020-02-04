216f61775114dc3890efcf7a81a8ce10843f7a17
Sky9th <weitianxu@qq.com>
2020-02-04 11:45:10 +0800

    [+] 菜单管理完善

--- app/common/model/sys/Menu.php
+++ app/common/model/sys/Menu.php
@@ -6,6 +6,7 @@ use app\common\model\Sys;
 class Menu extends Sys {
 
     protected $table = 'sys_menu';
-    protected $visible = ['id','pid','title','icon','path','visible'];
+    protected $visible = ['id','type','pid','title','permission','icon','path','visible','sort'];
+    public $order = 'sort desc, id desc';
 
 }
