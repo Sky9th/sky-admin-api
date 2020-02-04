1043ba34bfcfb8e31398a741e909f36d4b9daff5
Sky9th <weitianxu@qq.com>
2020-02-04 11:12:40 +0800

    [+] 路由管理接口完善

--- app/common/model/sys/Route.php
+++ app/common/model/sys/Route.php
@@ -6,7 +6,8 @@ use app\common\model\Sys;
 class Route extends Sys {
 
     protected $table = 'sys_route';
-    protected $visible = ['id','pid','name','path','component','component_path'];
+    protected $visible = ['id','pid','name','title','permission','path','component','component_path'];
+    public $order = 'sort desc, id desc';
 
     public function getMetaAttr ($value, $data) {
         return ['title'=>$data['title'], 'cache'=>boolval($data['cache'])];
