5cedbc52701917c147955d3917b432c3b47be8f2
Sky9th <weitianxu@qq.com>
2020-02-05 22:54:26 +0800

    [+] Sky9th.cn相关项目管理接口

--- app/admin/route/admin.php
+++ app/admin/route/admin.php
@@ -10,6 +10,7 @@
 // +----------------------------------------------------------------------
 use think\facade\Route;
 
+Route::rest('structure', ['GET', 'structure', 'structure']);
 Route::rest('delete', ['DELETE', '<id?>', 'delete']);
 
 Route::group(function () {
@@ -18,15 +19,18 @@ Route::group(function () {
 
 // 需要经过登录验证的接口，通过Auth中间件做登录鉴权
 Route::group(function () {
-    Route::rule('menu/indexByRoleId/:role_id', 'menu/indexByRoleId');
-    Route::rule('user/getPermissionInfo', 'user/getPermissionInfo');
-    Route::rule('user/indexByRoleId/:role_id', 'user/indexByRoleId');
-    Route::rule('role/savePermission', 'role/savePermission');
-    Route::rule('role/modifyUser', 'role/modifyUser');
-    Route::rule('role/indexByUserId/:user_id', 'role/indexByUserId');
-    Route::resource('roles', 'role');
-    Route::resource('menus', 'menu');
-    Route::resource('apis', 'api');
-    Route::resource('users', 'user');
-    Route::resource('routes', 'route');
+    Route::rule('menu/indexByRoleId/:role_id', 'sys.menu/indexByRoleId');
+    Route::rule('user/getPermissionInfo', 'sys.user/getPermissionInfo');
+    Route::rule('user/indexByRoleId/:role_id', 'sys.user/indexByRoleId');
+    Route::rule('role/savePermission', 'sys.role/savePermission');
+    Route::rule('role/modifyUser', 'sys.role/modifyUser');
+    Route::rule('role/indexByUserId/:user_id', 'sys.role/indexByUserId');
+    Route::resource('roles', 'sys.role');
+    Route::resource('menus', 'sys.menu');
+    Route::resource('apis', 'sys.api');
+    Route::resource('users', 'sys.user');
+    Route::resource('routes', 'sys.route');
+
+
+    Route::resource('projects', 'sky9th.project');
 })->middleware(\app\common\middleware\Auth::class);
