e006945cf2694f379b04b7a79fd8f70a0a4544e0
Sky9th <weitianxu@qq.com>
2020-02-12 17:54:07 +0800

    [!] bugfix

--- app/common/model/common/User.php
+++ app/common/model/common/User.php
@@ -9,18 +9,28 @@
 namespace app\common\model\common;
 
 use app\common\model\Common;
+use think\Model;
 
 class User extends Common
 {
 
     protected $table = 'common_user';
     protected $resultSetType = 'collection';
+    protected $readonly = ['username'];
+    protected $visible = ['id','type','nickname','realname','phone','mial','username','last_login_time','status'];
     protected $type = [
         'last_login' => 'timestamp',
     ];
 
     public function roles() {
         return $this->belongsToMany('app\common\model\sys\Role', 'sys_user_relation_role', 'role_id','user_id');
+    }
+
+    public function setPasswordAttr ($value, $data) {
+        $seed =  rand(1000, 9999);
+        $this->set('seed', $seed);
+        $encrypt_key = config('static.password_encrypt_key');
+        return md5($value.$seed.$encrypt_key);
     }
 
 }
