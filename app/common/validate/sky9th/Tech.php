f46241b99a828696d56bd50bb1cdfdee857aada6
Sky9th <weitianxu@qq.com>
2020-02-12 12:22:26 +0800

    [!] Sky9th.cn技术栈 bugfix

--- app/common/validate/sky9th/Tech.php
+++ app/common/validate/sky9th/Tech.php
@@ -8,7 +8,7 @@ class Tech extends Common {
     protected $rule = [
         'title|标题' => 'require|length:2,20',
         'version|简介' => 'require|length:0,255',
-        'link|链接' => 'require|length:0,255',
+        'link|链接' => 'require|url|length:0,255',
     ];
 
     protected $scene = [
