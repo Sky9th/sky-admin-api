70eb4a7d951d51da1ae482d458a36f4d700b5e06
Sky9th <weitianxu@qq.com>
2020-02-13 16:06:22 +0800

    [+] 接口关联菜单

--- app/admin/route/admin.php
+++ app/admin/route/admin.php
@@ -19,16 +19,22 @@ Route::group(function () {
 
 // 需要经过登录验证的接口，通过Auth中间件做登录鉴权
 Route::group(function () {
-    Route::rule('menu/indexByRoleId/:role_id', 'sys.menu/indexByRoleId');
-    Route::rule('user/getPermissionInfo', 'sys.user/getPermissionInfo');
-    Route::rule('user/indexByRoleId/:role_id', 'sys.user/indexByRoleId');
+    Route::resource('roles', 'sys.role');
     Route::rule('role/savePermission', 'sys.role/savePermission');
     Route::rule('role/modifyUser', 'sys.role/modifyUser');
     Route::rule('role/indexByUserId/:user_id', 'sys.role/indexByUserId');
-    Route::resource('roles', 'sys.role');
+
     Route::resource('menus', 'sys.menu');
+    Route::rule('menu/indexByRoleId/:role_id', 'sys.menu/indexByRoleId');
+
     Route::resource('apis', 'sys.api');
+    Route::rule('api/indexByMenuId/:menu_id', 'sys.api/indexByMenuId');
+    Route::rule('api/modifyMenu', 'sys.api/modifyMenu');
+
     Route::resource('users', 'sys.user');
+    Route::rule('user/getPermissionInfo', 'sys.user/getPermissionInfo');
+    Route::rule('user/indexByRoleId/:role_id', 'sys.user/indexByRoleId');
+
     Route::resource('routes', 'sys.route');
 
 
--- app/common/model/sys/Api.php
+++ app/common/model/sys/Api.php
@@ -8,4 +8,8 @@ class Api extends Sys {
     protected $table = 'sys_api';
     protected $visible = ['id','title','path','method','description'];
 
+    public function menus () {
+        return $this->belongsToMany('menu', 'sys_menu_relation_api','menu_id','api_id');
+    }
+
 }
