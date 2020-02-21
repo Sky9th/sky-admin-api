5084d56f2eb2a93642c3b709598344c1fb1a408a
Sky9th <weitianxu@qq.com>
2020-02-21 18:17:54 +0800

    [*] 允许跨域

--- app/common/middleware/AllowCrossDomain.php
+++ app/common/middleware/AllowCrossDomain.php
@@ -4,7 +4,8 @@ namespace app\common\middleware;
 class AllowCrossDomain extends \think\middleware\AllowCrossDomain
 {
     protected $header = [
-        'Access-Control-Allow-Origin' => '*.sky9th.cn',
+        //'Access-Control-Allow-Origin' => '*.sky9th.cn,111.231.139.51',
+        'Access-Control-Allow-Origin' => '*',
         'Access-Control-Allow-Credentials' => 'true',
         'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers' => 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With, Noncestr, Session-Key, Signature, Timestamp',
